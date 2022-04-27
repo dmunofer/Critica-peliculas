21287df4dcc9816ecdd249403d93f1b9cf25e2f5
dmunofer <diego.fmz@hotmail.com>
2022-04-27 23:13:34 +0200

    media ponderada

--- criticapeliculas.py
+++ criticapeliculas.py
@@ -1,3 +1,15 @@
 import matplotlib.pyplot as plt 
 import pandas as pd 
 import seaborn as sns
+
+
+def mediapond(datos):
+    suma=0
+    for i in range (len(datos["Cantidad de volantes (Ni)"])):
+        suma+= datos["Cantidad de volantes (Ni)"][i]*datos["Opinion xi"][i]
+    
+    num=0
+    for i in datos["Cantidad de volantes (Ni)"]:
+        num+=i
+
+    return suma/num
